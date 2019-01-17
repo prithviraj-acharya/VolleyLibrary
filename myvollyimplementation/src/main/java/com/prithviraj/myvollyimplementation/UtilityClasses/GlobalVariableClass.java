30dfb42fca186f9d284d56fc216e59f3d5c941b5
Arpan Datta <arpandatta72@gmail.com>
2019-01-18 01:26:02 +0530

    Added initialization to request list

--- myvollyimplementation/src/main/java/com/prithviraj/myvollyimplementation/UtilityClasses/GlobalVariableClass.java
+++ myvollyimplementation/src/main/java/com/prithviraj/myvollyimplementation/UtilityClasses/GlobalVariableClass.java
@@ -4,6 +4,8 @@ import com.android.volley.Request;
 
 import java.util.List;
 
+import java.util.ArrayList;
+
 public class GlobalVariableClass {
 
     public List<Request> requestList = new ArrayList<Request>();
